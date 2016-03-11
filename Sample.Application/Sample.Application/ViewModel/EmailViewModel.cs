7aa6c629d569ac9657b351720084bf925ed193dc
devendrachhantyal <chhantyal.devan@gmail.com>
2016-03-11 11:45:47 +0545

    Initial Commit

---
+++ Sample.Application/Sample.Application/ViewModel/EmailViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Sample.Application.Context;
+using Sample.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace Sample.Application.ViewModel
+{
+    public class EmailViewModel
+    {
+
+        private AppDbConnection db = new AppDbConnection();
+        public IEnumerable<SelectListItem> Title { get; set; }
+        public Email Email { get; set; }
+
+
+        public EmailViewModel(Email email)
+        {
+            Email = email;
+            Title = PopulateTitle();
+        }
+        private IEnumerable<SelectListItem> PopulateTitle()
+        {
+            var titleQuery = db.EmailTemplates.OrderBy(t => t.Id);
+            return new SelectList(titleQuery, "Title", "Id");
+        }
+    }
+}
